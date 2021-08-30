f856375f9ef2ab1daa9afe4e9cad523912ea96e4
Piotr Gorski <lucjan.lucjanov@gmail.com>
2021-08-30 13:08:40 +0200

    port: print fsync count for bootchart
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>
    Signed-off-by: ninez <johnstonljordan@gmail.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -1077,6 +1077,9 @@ blk_qc_t submit_bio(struct bio *bio)
 		} else {
 			task_io_account_read(bio->bi_iter.bi_size);
 			count_vm_events(PGPGIN, count);
+
+		if (bio->bi_opf & REQ_PREFLUSH)
+			current->fsync_count++;
 		}
 	}
 
--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1043,6 +1043,7 @@ struct task_struct {
 	/* Cached requested key. */
 	struct key			*cached_requested_key;
 #endif
+	int fsync_count;
 
 	/*
 	 * executable name, excluding path.
