8b2dabde5c92efe460f659ce6294cfd143a5bd09
Oleksandr Natalenko <oleksandr@natalenko.name>
2021-10-08 09:23:59 +0200

    mac80211: rate: replace WARN_ON with pr_warn
    This is a hack.
    
    Work around the following warning:
    
    ```
    WARNING: CPU: 2 PID: 656 at net/mac80211/rate.c:682 ieee80211_get_tx_rates+0x604/0xb50 [mac80211]
    …
    Call Trace:
     <IRQ>
     ath_tx_start+0x122/0x2d0 [ath9k]
     ath9k_tx+0xd5/0x220 [ath9k]
     __ieee80211_tx+0x2ea/0x3b0 [mac80211]
     ieee80211_tx+0x11a/0x150 [mac80211]
     ieee80211_tx_pending+0xc2/0x2f0 [mac80211]
     tasklet_action_common.constprop.0+0x137/0x190
     __do_softirq+0xd1/0x2c6
     do_softirq+0x79/0xa0
     </IRQ>
     __local_bh_enable_ip+0x57/0x70
     irq_forced_thread_fn+0x7b/0x90
     irq_thread+0x203/0x2b0
     kthread+0x1b1/0x1e0
     ret_from_fork+0x22/0x30
    ```
    
    and turn it into a simple message.
    
    This happens with the following hardware:
    
    ```
    02:00.0 Network controller: Qualcomm Atheros AR9462 Wireless Network Adapter (rev 01)
    ```
    
    running in AP mode using hostapd.
    
    Signed-off-by: Oleksandr Natalenko <oleksandr@natalenko.name>
    Signed-off-by: ninez <johnstonljordan@gmail.com>

--- net/mac80211/rate.c
+++ net/mac80211/rate.c
@@ -676,7 +676,9 @@ static void rate_fixup_ratelist(struct ieee80211_vif *vif,
 		}
 
 		if (rates[i].flags & IEEE80211_TX_RC_VHT_MCS) {
-			WARN_ON(ieee80211_rate_get_vht_mcs(&rates[i]) > 9);
+			if (unlikely(ieee80211_rate_get_vht_mcs(&rates[i]) > 9))
+				pr_warn("%s: ieee80211_rate_get_vht_mcs(&rates[%d]) == %d > 9\n",
+					__func__, i, ieee80211_rate_get_vht_mcs(&rates[i]));
 			continue;
 		}
 
