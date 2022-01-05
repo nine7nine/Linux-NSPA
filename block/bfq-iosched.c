586dc81e6789769a0d97bbc1e64ac8f4a1b5649f
Jan Kara <jack@suse.cz>
2022-01-05 15:36:34 +0100

    bfq: Simplify bfq_put_cooperator()
    All calls to bfq_setup_merge() are followed by bfq_merge_bfqqs() so
    there should be no chance for chaining several queue merges. And if
    chained queue merges were possible, then bfq_put_cooperator() would drop
    cooperator references without clearing corresponding bfqq->new_bfqq
    pointers causing possible use-after-free issues. Fix these problems by
    making bfq_put_cooperator() drop only the immediate bfqq->new_bfqq
    reference.
    
    CC: stable@vger.kernel.org
    Fixes: 36eca8948323 ("block, bfq: add Early Queue Merge (EQM)")
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: ninez <johnstonljordan@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -5334,22 +5334,16 @@ static void bfq_put_stable_ref(struct bfq_queue *bfqq)
 	bfq_put_queue(bfqq);
 }
 
+
+/*
+ * If this queue was scheduled to merge with another queue, be
+ * sure to drop the reference taken on that queue.
+ */
 static void bfq_put_cooperator(struct bfq_queue *bfqq)
 {
-	struct bfq_queue *__bfqq, *next;
-
-	/*
-	 * If this queue was scheduled to merge with another queue, be
-	 * sure to drop the reference taken on that queue (and others in
-	 * the merge chain). See bfq_setup_merge and bfq_merge_bfqqs.
-	 */
-	__bfqq = bfqq->new_bfqq;
-	while (__bfqq) {
-		if (__bfqq == bfqq)
-			break;
-		next = __bfqq->new_bfqq;
-		bfq_put_queue(__bfqq);
-		__bfqq = next;
+	if (bfqq->new_bfqq) {
+		bfq_put_queue(bfqq->new_bfqq);
+		bfqq->new_bfqq = NULL;
 	}
 }
 
