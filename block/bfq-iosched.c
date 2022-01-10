d4985d33f693340b3ef74876df8b7d9a11aeba00
Piotr Gorski <lucjan.lucjanov@gmail.com>
2022-01-10 20:34:11 +0100

    block,bfq: set bfq-lucjan branding
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>
    Signed-off-by: ninez <johnstonljordan@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -7464,6 +7464,7 @@ MODULE_ALIAS("bfq-iosched");
 static int __init bfq_init(void)
 {
 	int ret;
+	char msg[60] = "BFQ I/O-scheduler: BFQ-lucjan v5.16";
 
 #ifdef CONFIG_BFQ_GROUP_IOSCHED
 	ret = blkcg_policy_register(&blkcg_policy_bfq);
@@ -7494,6 +7495,11 @@ static int __init bfq_init(void)
 	ret = elv_register(&iosched_bfq_mq);
 	if (ret)
 		goto slab_kill;
+
+#ifdef CONFIG_BFQ_GROUP_IOSCHED
+	strcat(msg, " (with cgroups support)");
+#endif
+	pr_info("%s", msg);
 
 	return 0;
 
