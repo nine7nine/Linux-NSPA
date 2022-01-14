9c9a2a03634ed32c7041f225421363d3a44c64d0
Piotr Gorski <lucjan.lucjanov@gmail.com>
2022-01-14 13:55:32 +0100

    Revert "bfq: fix use-after-free in bfq_dispatch_request"
    This reverts commit a8679b6a74be63fe0030f8add7ef50e07204ec6f.
    
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>
    Signed-off-by: ninez <johnstonljordan@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -5227,7 +5227,7 @@ static struct request *bfq_dispatch_request(struct blk_mq_hw_ctx *hctx)
 	struct bfq_data *bfqd = hctx->queue->elevator->elevator_data;
 	struct request *rq;
 	struct bfq_queue *in_serv_queue;
-	bool waiting_rq, idle_timer_disabled = false;
+	bool waiting_rq, idle_timer_disabled;
 
 	spin_lock_irq(&bfqd->lock);
 
@@ -5237,10 +5237,9 @@ static struct request *bfq_dispatch_request(struct blk_mq_hw_ctx *hctx)
 
 	rq = __bfq_dispatch_request(hctx);
 
-	if (in_serv_queue == bfqd->in_service_queue) {
-		idle_timer_disabled =
-			waiting_rq && !bfq_bfqq_wait_request(in_serv_queue);
-	}
+	idle_timer_disabled =
+		waiting_rq && !bfq_bfqq_wait_request(in_serv_queue);
+
 	spin_unlock_irq(&bfqd->lock);
 
 	bfq_update_dispatch_stats(hctx->queue, rq, in_serv_queue,
